0199335a10c7e829f69093fe9905b8d6fb2bf66e
Andrew Ek <andrewek@gmail.com>
2020-02-12 07:15:54 -0600

    Settings section complete
    We can now fully set (and persist) PPM-specific settings through the admin dashboard.

--- ppm-woocommerce.php
+++ ppm-woocommerce.php
@@ -4,62 +4,100 @@
    * Plugin URI: https://github.com/PPM-Fulfillment/ppm-woocommerce
    * Description: Fulfill your WooCommerce orders through PPM Fulfillment
    * Author: Andrew Ek
+   * Text Domain: ppm-woo
    */
 
-function ppm_settings_init() {
-  // Create the options page
-  add_action("admin_menu", "ppm_settings_page");
+define("PPM_WOOCOMMERCE_VERSION", "1.0.0");
 
-  add_options_page(
-    "PPM Settings Page",
-    "PPM Settings Page",
+function ppm_woo_admin_menu() {
+  add_menu_page(
+    __("PPM Options", "ppm-woo"),
+    __("PPM Options", "ppm-woo"),
     "manage_options",
-    "ppm-settings",
-    "ppm_settings_page"
+    "ppm-woo-options",
+    "ppm_woo_contents",
   );
+}
 
+function ppm_woo_contents() {
+  ?>
+  <h1>PPM Fulfillment Options</h1>
+  <form method="POST" action="options.php">
+  <?php
+  settings_fields("ppm-woo-options");
+  do_settings_sections("ppm-woo-options");
+  submit_button();
+  ?>
+  </form>
+  <?php
+}
 
-  // Create an option for the PPM API Key under the "ppm" namespace
-  register_setting("ppm", "ppm_api_key");
-
+function ppm_woo_settings_init() {
   add_settings_section(
-    "ppm_config_section",
-    "PPM Fulfillment",
-    "ppm_config_section_cb",
-    "ppm"
+    "ppm_woo_settings_section",
+    __("PPM Configuration", "ppm-woo"),
+    "ppm_settings_section_callback_function",
+    "ppm-woo-options"
   );
 
   add_settings_field(
-    "ppm_api_key",
-    "PPM API Key",
-    "ppm_api_key_cb",
-    "ppm",
-    "ppm_config_section"
+    "ppm_woo_api_key",
+    __("PPM Fulfillment API Key", "ppm-woo"),
+    "ppm_woo_api_key_markup",
+    "ppm-woo-options",
+    "ppm_woo_settings_section"
+  );
+  add_settings_field(
+    "ppm_woo_owner_code",
+    __("PPM Fulfillment Owner Code", "ppm-woo"),
+    "ppm_woo_owner_code_markup",
+    "ppm-woo-options",
+    "ppm_woo_settings_section"
+  );
+  add_settings_field(
+    "ppm_woo_api_url",
+    __("PPM Fulfillment API URL", "ppm-woo"),
+    "ppm_woo_api_url_markup",
+    "ppm-woo-options",
+    "ppm_woo_settings_section"
+  );
+
+  register_setting("ppm-woo-options", "ppm_woo_api_key");
+  register_setting("ppm-woo-options", "ppm_woo_owner_code");
+  register_setting("ppm-woo-options", "ppm_woo_api_url");
+}
+
+function ppm_settings_section_callback_function() {
+  echo "<p>Set your config values for PPM Fulfillment</p>";
+}
+
+function ppm_woo_api_key_markup() {
+  generic_field_markup("ppm_woo_api_key", "PPM Fulfillment API Key");
+}
+
+function ppm_woo_owner_code_markup() {
+  generic_field_markup("ppm_woo_owner_code", "PPM Fulfillment Owner Code");
+}
+
+function ppm_woo_api_url_markup() {
+  generic_field_markup(
+    "ppm_woo_api_url", 
+    "PPM Fulfillment API URL", 
+    "https://portal.ppmfulfillment.com/api/External/ThirdPartyOrders"
   );
 }
 
-/**
- * Register our Settings at large
- */
-add_action("admin_init", "ppm_settings_init");
-
-/**
- * Define our Config Section
- */
-function ppm_config_section_cb($args) {
+function generic_field_markup($field_id, $field_name, $default="") {
   ?>
-  echo "<p>PPM Fulfillment Configuration</p>";
+  <label for="<?php echo $field_id; ?>"><?php _e($field_name, "ppm-woo"); ?></label>
+  <input 
+    type="text" 
+    id="<?php echo $field_id; ?>" 
+    name="<?php echo $field_id; ?>"
+    value="<?php echo get_option($field_id, $default); ?>"
+  >
   <?php
 }
 
-/**
- * Define our API Key Form HTML
- */
-function ppm_api_key_cb() {
-  $setting = get_option("ppm_api_key");
-
-  ?>
-    <label for="ppm_api_key">
-    <input type="text" name="ppm_api_key" value="<?php echo isset($setting) ? esc_attr($setting) : ""; ?>">
-  <?php
-}
+add_action("admin_menu", "ppm_woo_admin_menu");
+add_action("admin_init", "ppm_woo_settings_init");
