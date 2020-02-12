c0e752000371ab29016b7e22376ec9fd80442ae0
Andrew Ek <andrew@unabridgedsoftware.com>
2020-02-11 21:38:15 -0600

    WIP Settings Page
    We're following documentation from these sources:
    
    + https://www.sitepoint.com/wordpress-settings-api-build-custom-admin-page/
    + https://developer.wordpress.org/plugins/settings/using-settings-api/
    
    Let's get the API Key, PPM API URL (drop-down), and Owner Code rocking and rolling.

--- ppm-woocommerce.php
+++ ppm-woocommerce.php
@@ -1,4 +1,65 @@
 <?php
   /**
    * Plugin Name: PPM Fulfillment - Woo Commerce
+   * Plugin URI: https://github.com/PPM-Fulfillment/ppm-woocommerce
+   * Description: Fulfill your WooCommerce orders through PPM Fulfillment
+   * Author: Andrew Ek
    */
+
+function ppm_settings_init() {
+  // Create the options page
+  add_action("admin_menu", "ppm_settings_page");
+
+  add_options_page(
+    "PPM Settings Page",
+    "PPM Settings Page",
+    "manage_options",
+    "ppm-settings",
+    "ppm_settings_page"
+  );
+
+
+  // Create an option for the PPM API Key under the "ppm" namespace
+  register_setting("ppm", "ppm_api_key");
+
+  add_settings_section(
+    "ppm_config_section",
+    "PPM Fulfillment",
+    "ppm_config_section_cb",
+    "ppm"
+  );
+
+  add_settings_field(
+    "ppm_api_key",
+    "PPM API Key",
+    "ppm_api_key_cb",
+    "ppm",
+    "ppm_config_section"
+  );
+}
+
+/**
+ * Register our Settings at large
+ */
+add_action("admin_init", "ppm_settings_init");
+
+/**
+ * Define our Config Section
+ */
+function ppm_config_section_cb($args) {
+  ?>
+  echo "<p>PPM Fulfillment Configuration</p>";
+  <?php
+}
+
+/**
+ * Define our API Key Form HTML
+ */
+function ppm_api_key_cb() {
+  $setting = get_option("ppm_api_key");
+
+  ?>
+    <label for="ppm_api_key">
+    <input type="text" name="ppm_api_key" value="<?php echo isset($setting) ? esc_attr($setting) : ""; ?>">
+  <?php
+}
