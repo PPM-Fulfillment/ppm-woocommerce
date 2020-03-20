98c82014da5857a00453b0de38427e90b9503ef6
Andrew Ek <andrewek@gmail.com>
2020-03-20 00:17:16 -0500

    Completed tracking endpoint
    COMPLETE

--- ppm-shipping-api-endpoint.php
+++ ppm-shipping-api-endpoint.php
@@ -35,11 +35,82 @@
     $order_id = $request["OrderId"];
     $tracking_number = $request["TrackingNumber"];
     $carrier = $request["Carrier"];
+
+    $order = wc_get_order($order_id);
+
+    if(empty($order)) {
+        error_log("BOMBED OUT ON ORDER: " . $order_id);
+        $response = new WP_REST_Response(array("success" => FALSE));
+        $response->set_status(404);
+        return $response; 
+    }
     
-    error_log("REQUEST RECEIVED!");
-    error_log($order_id);
-    error_log($tracking_number);
-    error_log($carrier);
+    $carrier_string = "\nCarrier: " . $carrier;
+    $tracking_string = "Tracking Number: " . $tracking_number;
+    $note = "Received Tracking Information." . $carrier_string . "\n" . $tracking_string;
+    $order->add_order_note($note);
+    
+    // Get all order items so we can match to line items in POST body
+    $order_items = array();
+    foreach($order->get_items() as $item_id => $item) {
+        $product = $item->get_product();
+
+        if(is_object($product)) {
+            $ppm_sku = get_post_meta($product->get_id(), "ppm_sku", true);
+            $sku = $product->get_sku();
+
+            // Set fallback SKU for querying
+            if(!empty($ppm_sku)) {
+                $sku = $ppm_sku;
+            }
+
+            $name = $item["name"];
+            $order_items[] = array(
+                "sku" => $sku,
+                "name" => $name,
+            );
+        }
+    }
+
+    $line_items = $request["LineItems"];
+
+    // { "ProductId": "", "Quantity": "", "SerialNumber": "", "LotNumber": ""}
+    foreach($line_items as $item) {
+        $product_id = $item["ProductId"];
+
+        $tmp_product_index = array_search($product_id, array_column($order_items, "sku"));
+
+        if($tmp_product_index === false) {
+            $note = "WARNING: " . $tracking_string . 
+                "\n" . "Received submission for product " . 
+                $product_id . ". Not found. Quantity: " . $item["Quantity"];
+            $order->add_order_note($note);
+            continue;
+        }
+
+        $tmp_product = $order_items[$tmp_product_index];
+
+        $quantity = $item["Quantity"];
+        $serial_number = $item["SerialNumber"];
+        $lot_number = $item["LotNumber"];
+
+        $product_name = $tmp_product["name"] . " (" . $product_id . ")";
+        $qty_string = "\nQuantity: " . $quantity;
+        
+        $lot_string = "\nLot Number: None";
+        if(!empty($lot_number)) {
+            $lot_string = "\nLot Number: " . $lot_number;
+        }
+
+        $serial_string = "\nSerial Number: None";
+        if(!empty($serial_number)) {
+            $serial_string = "\nSerial Number: " . $serial_number;
+        }
+
+        $note_text = $tracking_string . "\n" . $product_name . $qty_string . $lot_string . $serial_string;
+
+        $order->add_order_note($note_text);
+    }
 
     $response = new WP_REST_Response(array("success" => TRUE));
     $response->set_status(200);
